4206f5910234cb03cab60bd15539f3be060dbc0e
nikitamakarevich <nikita-makarevich@mail.ru>
2022-01-30 16:05:17 +0300

    Screen is ready. Need to finish code part

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -9,13 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var questionLable: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    @IBOutlet weak var bubblesIV: UIImageView!
+    
+    let quiz = [
+        ["Four + Two equal to Six", "True"],
+        ["Nine / Three equal Two", "False"],
+        ["Five + Three equal Eight", "True"]
+    ]
+    
+    var questionNumber = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        // Nothing
+        bubblesIV.transform = CGAffineTransform(scaleX: 1, y: -1)
+        progressBar.layer.cornerRadius = 4
+        progressBar.clipsToBounds = true
+        progressBar.layer.sublayers![1].cornerRadius = 2
+        progressBar.subviews[1].clipsToBounds = true
+        
+        
+        updateUI()
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Right")
+        }
+        else {
+            print("Wrong")
+        }
+        
+        if questionNumber < quiz.count-1 {
+            questionNumber+=1
+            
+        }
+        else {
+            questionNumber = 0
+        }
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLable.text = quiz[questionNumber][0]
+    }
+    
 }
 
